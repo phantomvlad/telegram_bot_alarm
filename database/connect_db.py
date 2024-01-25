a7f16a068da59c54165f6d9e56e3ddcaaf01e561
PhantomVlad <mmvlad99@mail.ru>
2024-01-25 19:41:09 +0300

    fix db

--- database/connect_db.py
+++ database/connect_db.py
@@ -3,6 +3,6 @@ from sqlalchemy import create_engine
 from config.config import Config
 
 async def connect_db(config: Config):
-    engine = create_engine(f"postgresql+psycopg2://{config.db.db_user}:{config.db.db_password}@localhost/{config.db.database}")
+    engine = create_engine(f"postgresql+psycopg2://{config.db.db_user}:{config.db.db_password}@{config.db.db_host}:{config.db.db_port}/{config.db.database}")
     engine.connect()
     print('test', engine)
